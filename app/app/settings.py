95c9be7e6239db3d69f3f944ae57b415607e4460
Hitsam Tiammar <hitsam@tetherfi.com>
2024-08-24 02:32:06 +0700

    add seeder and initiate env variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,11 +90,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test-django', 
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'NAME': env('DATABASE_NAME'), 
+        'USER': env('DATABASE_USERNAME'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
