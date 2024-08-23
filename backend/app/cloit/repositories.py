e4d473dcfc73a91f8147e4a6848ae5112d67d4ed
Hitsam Tiammar <hitsam@tetherfi.com>
2024-08-24 02:19:11 +0700

    implement get all based on root id

--- backend/app/cloit/repositories.py
+++ backend/app/cloit/repositories.py
@@ -13,3 +13,29 @@ def get_all():
        return d
     except Exception:
         return None
+
+def get_all_root():
+    try:
+       d = MenuItem.objects.filter(parent_id__isnull = True)
+       return d
+    except Exception:
+        return None
+    
+def get_all_data(id, depth = 1):
+    try:
+        curr_data = MenuItem.objects.get(pk = id)
+        children = MenuItem.objects.filter(parent_id = id)
+        children_data = []
+
+        for child in children:
+            children_data.append(get_all_data(child.menu_id, depth + 1))
+
+        return {
+            'id': curr_data.menu_id,
+            'parent': curr_data.parent_id,
+            'name': curr_data.name,
+            'children': children_data,
+            'depth': depth
+        }
+    except Exception:
+        return []
--- backend/app/cloit/urls.py
+++ backend/app/cloit/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.insert, name='add'),
-    path('view/', views.views_data, name='view'),
+    path('view-root/', views.views_data_root, name='view_root'),
+    path('view-data/<id>', views.views_data_data, name='view_data'),
     path('update/', views.update, name='update'),
     path('delete/<id>', views.delete, name='delete')
 ]
