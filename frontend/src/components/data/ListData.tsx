9a1db82da3c1c93a08b5a5c96eef9b287134b352
Hitsam Tiammar <Hitsam Tiammar>
2024-08-24 21:24:00 +0700

    expand data whenever new data added

--- frontend/src/components/data/ListData.tsx
+++ frontend/src/components/data/ListData.tsx
@@ -56,6 +56,7 @@ export function ListData({ onCancelItemSelected = noop, onItemSelected = noop, d
 
     const onAddBtnClicked = (e:React.MouseEvent) => {
         e.stopPropagation()
+        setExpanded(true)
         setChildrenData([...childrenData, {
             children: [],
             depth: 0,
